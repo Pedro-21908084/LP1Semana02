e9f6ed4e4bd0c9f5613cb85e0a570fac0e20b81e
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-02 18:34:53 +0000

    Exercise 4 read player inputs

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,29 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //declaration of variables
+            float pi = 3.1415926f;
+            float height;
+            string sHeight;
+            float radius;
+            string sRadius;
+            float volume = 0;
+            float surfaceArea = 0;
+
+            //player input
+            Console.WriteLine("Hello, please tell me the cylinder's height:");
+            sHeight = Console.ReadLine();
+            Console.WriteLine("Good, now tell me the cylinder's radius:");
+            sRadius = Console.ReadLine();
+
+            //convert string to float
+
+            //calculations
+
+            //print results
+            Console.WriteLine($"A cylinder with a height of {sHeight}, and a" +
+            $" radius of {sRadius}, has a volume of {volume} and the surface" +
+            $" area of {surfaceArea}.");
         }
     }
 }
