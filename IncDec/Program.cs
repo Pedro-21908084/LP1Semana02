bcd07d944ea3197689105abe6e55dcac633b2972
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-02 18:13:02 +0000

    Exercicio 1

---
+++ IncDec/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 0;
+
+            //espero que x=2 e y=2
+            y= ++x;
+
+            Console.WriteLine($"x = {x} e y = {y}");
+        }
+    }
+}
