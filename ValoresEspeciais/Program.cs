7c76f4892bfd9f1b96dba63ae36d60af264672a5
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-02 19:04:00 +0000

    Exercise 6 first half

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte
+            Console.WriteLine($"Byte:");
+            Console.WriteLine($"    Max: {byte.MaxValue}");
+            Console.WriteLine($"    Min: {byte.MinValue}");
+
+            //sbyte
+            Console.WriteLine($"Sbyte:");
+            Console.WriteLine($"    Max: {sbyte.MaxValue}");
+            Console.WriteLine($"    Min: {sbyte.MinValue}");
+
+            //short
+            Console.WriteLine($"Short:");
+            Console.WriteLine($"    Max: {short.MaxValue}");
+            Console.WriteLine($"    Min: {short.MinValue}");
+
+            //ushort
+            Console.WriteLine($"Ushort:");
+            Console.WriteLine($"    Max: {ushort.MaxValue}");
+            Console.WriteLine($"    Min: {ushort.MinValue}");
+            
+            //int
+            Console.WriteLine($"Int:");
+            Console.WriteLine($"    Max: {int.MaxValue}");
+            Console.WriteLine($"    Min: {int.MinValue}");
+
+            //uint
+            Console.WriteLine($"Uint:");
+            Console.WriteLine($"    Max: {uint.MaxValue}");
+            Console.WriteLine($"    Min: {uint.MinValue}");
+
+            //long
+            Console.WriteLine($"Long:");
+            Console.WriteLine($"    Max: {long.MaxValue}");
+            Console.WriteLine($"    Min: {long.MinValue}");
+
+            //ulong
+            Console.WriteLine($"Ulong:");
+            Console.WriteLine($"    Max: {ulong.MaxValue}");
+            Console.WriteLine($"    Min: {ulong.MinValue}");
+
+            //char
+            Console.WriteLine($"Char:");
+            Console.WriteLine($"    Max: {char.MaxValue}");
+            Console.WriteLine($"    Min: {char.MinValue}");
+
+            //float
+            Console.WriteLine($"Float:");
+            Console.WriteLine($"    Max: {float.MaxValue}");
+            Console.WriteLine($"    Min: {float.MinValue}");
+
+            //double
+            Console.WriteLine($"Double:");
+            Console.WriteLine($"    Max: {double.MaxValue}");
+            Console.WriteLine($"    Min: {double.MinValue}");
+
+        }
+    }
+}
