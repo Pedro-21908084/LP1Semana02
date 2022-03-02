88660afbc387d6746a81098e3a63ac1fa0f0baa2
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-02 22:58:15 +0000

    Added comments

--- Piramide/Program.cs
+++ Piramide/Program.cs
@@ -13,12 +13,14 @@ namespace Piramide
             //Read player input
             Console.WriteLine("Tell me the tree size:");
             input = Console.ReadLine();
+            //convert to int
             intInput = int.Parse(input);
 
             //Calculate size
             size = ImparPorIndex(intInput);
 
             //Loop to print tree
+            Console.WriteLine("");
             for (int i = 1; i <= intInput; i++)
             {
                 lineSize = ImparPorIndex(i);
@@ -26,9 +28,7 @@ namespace Piramide
                 for (int j = 0; j < size; j++)
                 {
                     if (j >= (size / 2 - lineSize / 2) && j < size / 2 + lineSize / 2 + 1)
-                    {
                         Console.Write("*");
-                    }
                     else
                         Console.Write(" ");
                 }
@@ -36,6 +36,11 @@ namespace Piramide
             }
         }
 
+        /// <summary>
+        /// returns an odd number in the index x
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         static int ImparPorIndex(int x)
         {
             return 1 + (x - 1) * 2;
