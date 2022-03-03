da28a730a60374fef671225b515bfa79684c9315
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-03 14:36:17 +0000

    Exercise 18

---
+++ Exercise18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration of variables
+            string input, response;
+            int charCount = 0;
+
+            //read input from player
+            Console.WriteLine("Write a sentence:");
+            input = Console.ReadLine();
+
+            //count char in input
+            charCount = input.Length;
+
+            //chose correct response
+            if (charCount >= 0 && charCount < 5)
+                response = "That's not very meaningful, is it?";
+            else if (charCount <= 20)
+                response = "Ok, if you say so.";
+            else
+                response = "Too long to read.";
+
+            //print response
+            Console.WriteLine(response);
+
+        }
+    }
+}
