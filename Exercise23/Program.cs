ba327948c45f1a8689071df74315f8560578afe2
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-03 15:13:33 +0000

    Exercise 23 use of return

--- Exercise23/Program.cs
+++ Exercise23/Program.cs
@@ -8,41 +8,37 @@ namespace Exercise23
         {
             //declaration of variables
             double arg1, arg2, result;
-            bool doCalculations = true;
 
             //check the number of arguments given and finish if not enough
             if (args.Length != 3)
             {
-                doCalculations = false;
                 Console.WriteLine("Not enough arguments given.");
+                return;
             }
-            if (args.Length > 0 && !(args[0] == "+" || args[0] == "-" ||
-            args[0] == "x" || args[0] == "/" || args[0] == "p"))
+            if (!(args[0] == "+" || args[0] == "-" || args[0] == "x" ||
+            args[0] == "/" || args[0] == "p"))
             {
-                doCalculations = false;
                 Console.WriteLine("Operation not recognized.");
+                return;
             }
 
-            if (doCalculations)
+            //convert arguments to doubles
+            arg1 = double.Parse(args[1]);
+            arg2 = double.Parse(args[2]);
+
+            //do the calculation ask
+            result = args[0] switch
             {
-                //convert arguments to doubles
-                arg1 = double.Parse(args[1]);
-                arg2 = double.Parse(args[2]);
+                "+" => arg1 + arg2,
+                "-" => arg1 - arg2,
+                "/" => arg1 / arg2,
+                "x" => arg1 * arg2,
+                "p" => Math.Pow(arg1, arg2),
+                _ => 0,
+            };
+            //print result
+            Console.WriteLine(result);
 
-                //do the calculation ask
-                result = args[0] switch
-                {
-                    "+" => arg1 + arg2,
-                    "-" => arg1 - arg2,
-                    "/" => arg1 / arg2,
-                    "x" => arg1 * arg2,
-                    "p" => Math.Pow(arg1, arg2),
-                    _ => 0,
-                };
-                //print result
-                Console.WriteLine(result);
-
-            }
         }
     }
 }
