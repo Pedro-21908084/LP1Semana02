4b0742237afe3a6460d7ab5eb24fee0c4e4a815f
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-03 14:48:16 +0000

    Exercise 19

---
+++ Exercise19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration of variables
+            string inputHighScore, inputCorrentScore, response;
+            string respNew = "New high score!";
+            string respKeep = "Keep trying.";
+            int highScore, correntScore;
+
+            //read input of player
+            Console.WriteLine("Write (as intergers) the highscore of the game" +
+            " and the players corrent score:");
+            inputHighScore = Console.ReadLine();
+            inputCorrentScore = Console.ReadLine();
+
+            //check high score
+            highScore = int.Parse(inputHighScore);
+            correntScore = int.Parse(inputCorrentScore);
+            response = (correntScore > highScore) ? respNew : respKeep;
+
+            //print correct response
+            Console.WriteLine(response);
+
+        }
+    }
+}
